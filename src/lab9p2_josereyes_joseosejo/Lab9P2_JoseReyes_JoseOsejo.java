e7dc6400bbd7312cb2533b4ddc7b82ce9a22f2a9
jose13001 <josecarlos_01_30@unitec.edu>
2022-03-18 13:50:43 -0600

    tortura en duo

---
+++ src/lab9p2_josereyes_joseosejo/Lab9P2_JoseReyes_JoseOsejo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9p2_josereyes_joseosejo;
+
+/**
+ *
+ * @author josec
+ */
+public class Lab9P2_JoseReyes_JoseOsejo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
